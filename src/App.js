b8afa55f1c0faf2a95723f933a1451f0ad54a1ca
loannrzdev <loann.picard-blardone@epitech.eu>
2024-02-25 19:14:33 +0100

    Initial commit Add Hello Word with animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { TypeAnimation } from 'react-type-animation';
+import "./style/app.scss"
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TypeAnimation
+            sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Hello word ! ',
+                2000, // wait 1s before replacing "Mice" with "Hamsters"
+            ]}
+            wrapper="span"
+            speed={9}
+            style={{ fontSize: '2em', display: 'inline-block', color: 'white'}}
+            repeat={Infinity}
+        />
     </div>
   );
 }
